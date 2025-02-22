50cb9bb5a8e9dfddc00920690cd7b9fcc803d723
mbarizao <mbarizao07@gmail.com>
2025-02-22 18:31:35 -0300

    fix: Database Name

--- src/config/index.ts
+++ src/config/index.ts
@@ -17,7 +17,7 @@ const configuration = (): IConfig => ({
     port: Number(process.env.DATABASE_PORT) || 5432,
     username: process.env.DATABASE_USERNAME,
     password: process.env.DATABASE_PASSWORD,
-    name: process.env.DATABASE_NAME ,
+    name: process.env.DATABASE_NAME || 'WootConnect',
   }
 });
 
